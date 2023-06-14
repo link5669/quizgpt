16abbe03400893999935341d35b87af17f260e83
miacq <miles.acquaviva@publicissapient.com>
2023-06-14 15:05:36 -0400

    refactored directories

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header.js'
+
+function App() {
+  return (
+    <div className="App-header">
+        <Header/>
+    </div>
+  );
+}
+
+export default App;
