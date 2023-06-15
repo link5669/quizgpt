aac2e70b35488dc0f288f5d09d619ae731deba6d
Nolan Gelinas <nolgelin@publicisgroupe.net>
2023-06-15 13:44:34 -0400

    Create pages

---
+++ frontend/src/Pages/startScreen.tsx
@@ -0,0 +1,17 @@
+import AboutButton from "../Components/about";
+import { EnterTopic } from "../Components/enterTopic";
+
+export const StartScreen = () => {
+	return (
+		<div className="flex flex-col text-center h-full justify-evenly font-default">
+			<div className="flex flex-col">
+				<h1 className="text-4xl font-bold">quizify</h1>
+				<h2 className="font-semibold">a project by PS interns 2023</h2>
+			</div>
+			<div className="flex flex-col items-center">
+				<EnterTopic />
+				<AboutButton />
+			</div>
+		</div>
+	);
+};
