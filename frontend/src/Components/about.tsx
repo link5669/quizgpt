696e2a32e12d59688054940fa31d6e4af958c084
Nolan Gelinas <nolgelin@publicisgroupe.net>
2023-06-15 15:24:15 -0400

    Create hover-scale utility class

--- frontend/src/Components/about.tsx
+++ frontend/src/Components/about.tsx
@@ -1,7 +1,7 @@
 const AboutButton = () => {
 	return (
 		<button>
-			<div className="rounded-xl bg-gray-200">
+			<div className="rounded-xl bg-gray-200 hover-scale">
 				<p className="mx-4 my-2">about me</p>
 			</div>
 		</button>
