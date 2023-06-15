fe089e1d98964e285eb35712ae0342b24e465e93
Nolan Gelinas <nolgelin@publicisgroupe.net>
2023-06-15 13:44:24 -0400

    Add Hammersmith One font

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
+		fontFamily: {
+			default: ["Hammersmith One", "sans-serif"],
+		},
 		extend: {},
 	},
 	plugins: [],
