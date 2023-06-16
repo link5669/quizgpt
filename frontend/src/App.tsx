4bc1cb36f03b36fdd7b0cbb5811b3c1be0019496
Nolan Gelinas <nolgelin@publicisgroupe.net>
2023-06-16 10:43:15 -0400

    Test large question and answers on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,20 +5,33 @@ function App() {
 	return (
 		<div className="w-screen h-screen bg-gray-100">
 			{/* <StartScreen /> */}
-			<QuizScreen questionNumber={1} questionData={placeholderData} />
+			<QuizScreen questionNumber={0} questionData={placeholderData} />
 		</div>
 	);
 }
 
-const placeholderData = {
-	question: "Which of the following is NOT a popular attraction in Boston?",
-	answers: [
-		"Fenway Park",
-		"Freedom Trail",
-		"Statue of Liberty",
-		"USS Constitution",
-	],
-	correctAnswer: 2,
-};
+const placeholderData = [
+	{
+		question: "Which of the following is NOT a popular attraction in Boston?",
+		answers: [
+			"Fenway Park",
+			"Freedom Trail",
+			"Statue of Liberty",
+			"USS Constitution",
+		],
+		correctAnswer: 2,
+	},
+	{
+		question:
+			"Example really long question Example really long question Example really long question Example really long question Example really long question Example really long question Example really long question Example really long question Example really long question Example really long question Example really long question Example really long question Example really long question?",
+		answers: [
+			"Example really long answer Example really long answer Example really long answer Example really long answer Example really long answer ",
+			"Freedom Trail",
+			"Statue of Liberty",
+			"USS Constitution",
+		],
+		correctAnswer: 2,
+	},
+];
 
 export default App;
--- frontend/src/Pages/quizScreen.tsx
+++ frontend/src/Pages/quizScreen.tsx
@@ -2,20 +2,24 @@ import { QuestionData } from "./../../types/shared.d";
 
 interface Props {
 	questionNumber: number;
-	questionData: QuestionData;
+	questionData: QuestionData[];
 }
 
 export default function QuizScreen({ questionNumber, questionData }: Props) {
+	const currentQuestionData = questionData[questionNumber];
 	return (
 		<div className="flex flex-col gap-3 h-full font-default text-center py-12">
-			<p className="text-4xl">
-				{"Question #" + questionNumber + ": " + questionData.question}
+			<p className="text-4xl mx-8">
+				{"Question #" +
+					(questionNumber + 1) +
+					": " +
+					currentQuestionData.question}
 			</p>
 			<div className="flex flex-col justify-evenly items-center h-full">
-				{questionData.answers.map((ans) => {
+				{currentQuestionData.answers.map((ans) => {
 					return (
 						<button className="bg-gray-200 rounded-full w-[75%] py-4 hover-scale">
-							<p>{ans}</p>
+							<p className="text-2xl">{ans}</p>
 						</button>
 					);
 				})}
