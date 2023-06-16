5183a0f3efb834c12394fa0c69a1fe9431168399
Leila Baniassad <lbaniassad@gmail.com>
2023-06-16 16:20:37 -0400

    final friday commits, updated index.ts file

--- frontend/src/redux/index.ts
+++ frontend/src/redux/index.ts
@@ -1,21 +1,51 @@
-import { createStore } from 'redux';
+import { createSlice, PayloadAction, configureStore } from '@reduxjs/toolkit'
+import { QuestionData } from '../../types/shared'
 
-const counterReducer = (state = { counter: 0 }, action: { type: string; }) => {
-    if(action.type === 'INCREMENT') {
-        return {
-            counter: state.counter + 1,
-        };
+//Quiz state & slice: stores questions and the index the user is on
+interface QuizState {
+  currentQuestion: number,
+  questionData: QuestionData[] | null
+}
+
+//add topic
+const initialState: QuizState = {
+  currentQuestion: 0,
+  questionData: null
+}
+
+const quizSlice = createSlice({
+  name: 'quiz',
+  initialState,
+  reducers: {
+    setCurrentQuestion: (state, action: PayloadAction<number>) => {
+      state.currentQuestion = action.payload
+    },
+    setQuestionData: (state, action: PayloadAction<QuestionData[]>) => {
+        state.questionData = action.payload
     }
+  }
+})
 
-    if(action.type === 'DECREMENT') {
-        return {
-            counter: state.counter - 1,
-        };
+const scoreSlice = createSlice({
+    name: 'score',
+    initialState: 0,
+    reducers: {
+        increment: (state) => {
+            state += 1;
+        },
+        decrement: (state) => {
+            state -= 1;
+        }
     }
+})
 
-    return state;
-};
+const store = configureStore({
+    reducer: {quiz: quizSlice.reducer, score: scoreSlice.reducer}
+})
 
-const store = createStore(counterReducer);
+// Export the action creators
+export const { setCurrentQuestion } = quizSlice.actions;
+export const { increment, decrement} = scoreSlice.actions;
 
 export default store;
+
