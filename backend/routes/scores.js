2b18419035b894275e048d41302adb5af90c2fe3
link5669 <milesacq@icloud.com>
2023-06-20 16:24:14 -0400

    changed get to post

--- backend/routes/scores.js
+++ backend/routes/scores.js
@@ -3,6 +3,16 @@ import { writeScoreData } from "../scores/firebase.js";
 
 const router = express.Router();
 
+router.post("/", async (req, res) => {
+    try {
+      const { username, topic, score } = req.query;
+      writeScoreData(username, topic, score);
+      res.status(200).send("Posted score")
+    } catch {
+      res.status(500).send("Error occurred while posting data to the API");
+    }
+}); 
+
 router.get("/", async (req, res) => {
     try {
       const { username, topic, score } = req.query;
