72d537d2bce02652afc14c7d6ccb9c1fa2587069
Nolan Gelinas <nolgelin@publicisgroupe.net>
2023-06-20 09:53:56 -0400

    Add CORS

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import express from "express";
 import questionsRouter from "./routes/questions.js";
+import cors from "cors";
 const port = process.env.PORT || 5000;
 const app = express();
+
+const corsConfig = {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsConfig));
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Quizify API" });
