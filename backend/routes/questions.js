83c46af04e9803ff0fdcba99bedb69e09cc4638c
link5669 <milesacq@icloud.com>
2023-06-21 16:09:49 -0400

    added auth on backend

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -4,16 +4,21 @@ import fetchQuestions from "../GPT/getFormattedQuestions.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  if (!(req.query.topic && req.query.numQuestions && req.query.difficulty)) {
-    res.status(400).send("Missing parameters")
-  } else {
-    try {
-      const { topic, numQuestions, difficulty } = req.query;
-      const questions = await fetchQuestions(topic, numQuestions, difficulty);
-      res.json(JSON.parse(questions));
-    } catch {
-      res.status(500).send("Error occurred while fetching data from the API");
+  console.log(req)
+  if (req.rawHeaders[1].split(" ")[1] == "8v8an328v8bde17cabc7vnwe2n4kj") {
+    if (!(req.query.topic && req.query.numQuestions && req.query.difficulty)) {
+      res.status(400).send("Missing parameters")
+    } else {
+      try {
+        const { topic, numQuestions, difficulty } = req.query;
+        const questions = await fetchQuestions(topic, numQuestions, difficulty);
+        res.json(JSON.parse(questions));
+      } catch {
+        res.status(500).send("Error occurred while fetching data from the API");
+      }
     }
+  } else {
+    res.status(401).send("Unauthorized")
   }
 }); 
 
