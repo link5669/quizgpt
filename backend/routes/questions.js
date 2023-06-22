2320b9e06c9f3fae46ed0098239589cb266af689
link5669 <milesacq@icloud.com>
2023-06-22 13:23:41 -0400

    added logging

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -3,17 +3,21 @@ import fetchQuestions from "../GPT/getFormattedQuestions.js";
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  var allowedOrigin = false
+function validateOrigin(req) {
   Object.entries(req).forEach(element => {
     if (element[0] == 'rawHeaders') {
       const originString = element[1][element[1].indexOf("Host") + 1]
       if (originString.includes("quizify") || originString.includes("localhost") || originString.includes("azure")) {
-        allowedOrigin = true
+        return true
       }
     }
   })
-  if (allowedOrigin) {
+  return false
+}
+
+router.get("/", async (req, res) => {
+  console.log(req.rawHeaders)
+  if (validateOrigin(req)) {
     if (!(req.query.topic && req.query.numQuestions && req.query.difficulty)) {
       res.status(400).send("Missing parameters")
     } else {
