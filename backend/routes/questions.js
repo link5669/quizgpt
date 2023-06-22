12e7498dbe7a8d1fac4512865884488c0c28e397
Miles Acquaviva <milesacq@icloud.com>
2023-06-22 11:44:59 -0400

    Update questions.js

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -4,8 +4,6 @@ import fetchQuestions from "../GPT/getFormattedQuestions.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  console.log(req.rawHeaders[req.rawHeaders.indexOf("Authorization") + 1])
-  if (req.rawHeaders[req.rawHeaders.indexOf("Authorization") + 1].split(" ")[1] == "8v8an328v8bde17cabc7vnwe2n4kj") {
     if (!(req.query.topic && req.query.numQuestions && req.query.difficulty)) {
       res.status(400).send("Missing parameters")
     } else {
@@ -17,9 +15,6 @@ router.get("/", async (req, res) => {
         res.status(500).send("Error occurred while fetching data from the API");
       }
     }
-  } else {
-    res.status(401).send("Unauthorized")
-  }
 }); 
 
 export default router;
