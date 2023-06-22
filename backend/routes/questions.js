a1a2bbc50631f7ee7a19b8f3524544898e743c62
link5669 <milesacq@icloud.com>
2023-06-22 10:55:43 -0400

    improved auth

--- backend/routes/questions.js
+++ backend/routes/questions.js
@@ -4,8 +4,16 @@ import fetchQuestions from "../GPT/getFormattedQuestions.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  console.log(req.rawHeaders[req.rawHeaders.indexOf("Authorization") + 1])
-  if (req.rawHeaders[req.rawHeaders.indexOf("Authorization") + 1].split(" ")[1] == "8v8an328v8bde17cabc7vnwe2n4kj") {
+  var allowedOrigin = false
+  Object.entries(req).forEach(element => {
+    if (element[0] == 'rawHeaders') {
+      const originString = element[1][element[1].indexOf("Host") + 1]
+      if (originString.includes("quizify") || originString.includes("localhost")) {
+        allowedOrigin = true
+      }
+    }
+  })
+  if (req.rawHeaders[req.rawHeaders.indexOf("Authorization") + 1].split(" ")[1] == "8v8an328v8bde17cabc7vnwe2n4kj" && allowedOrigin) {
     if (!(req.query.topic && req.query.numQuestions && req.query.difficulty)) {
       res.status(400).send("Missing parameters")
     } else {
