756e0be248a7c8bb051b9530977aba69e8f8c943
nrgbistro <nolangelinas@gmail.com>
2023-06-25 10:01:43 -0400

    Fix true answer color

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,6 +2,6 @@ export const ERROR_TIMEOUT = 5000;
 export const ANSWER_TIMEOUT = 2000;
 export const CORRECT_COLOR = "bg-green-500";
 export const INCORRECT_COLOR = "bg-red-500";
-export const TRUE_COLOR = "bg-blue-300";
+export const TRUE_COLOR = "bg-true_answer";
 export const DEFAULT_NUM_QUESTIONS = 5;
 export const DEFAULT_DIFFICULTY = "medium";
